a74e3f35038b4b7231df3e13321d49f253ba7446
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-04 16:26:51 -0400

    changes

--- index.php
+++ index.php
@@ -10,7 +10,63 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+<?php
+      
 
+        $link = mysqli_connect('localhost', 'kay', 'dtycts16');
+
+        if (!$link) {
+            $output = 'Unable to connect to the Database Server.';
+            include'output.html.php';
+            exit();
+        }
+        if (!mysqli_set_charset($link, 'utf8')) {
+            $output = 'Unable to set database connection encoding.';
+            include'output.html.php';
+            exit();
+        }
+        if (!mysqli_select_db($link, 'kay')) {
+        
+            include'output.html.php';
+            exit();
+        }
        
+        include'output.html.php';
+        
+       
+        if(!mysqli_query($link, $sql))
+        {
+           
+            include 'output.html.php';
+            exit();
+        }
+        
+    
+        include 'output.html.php';
+      
+
+$servername = "localhost";
+$username = "kay";
+$password = "dtycts16";
+$dbname = "kay";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
     </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -26,88 +26,13 @@ and open the template in the editor.
         htmlspecialchars($username, ENT_QUOTES, 'UTF-8') . '!';
         }
       
-if (!isset($_REQUEST['firstname']))
-{
-include 'form.html.php';
-}
-else
-{
-  $username = $_REQUEST['username'];
-
-  if ($username == 'Kevin' and $lastname == 'Yank')
-  {
-$output =
- 'Welcome, oh glorious leader!';
-  }
-  else
-  {
-$output =
- 'Welcome to our web site, ' .
-        htmlspecialchars($username, ENT_QUOTES, 'UTF-8') . ' ' .
- 
-  
-include 'welcome.html.php';
-}
-
-        $link = mysqli_connect('localhost', 'kay', 'dtycts16');
-
-        if (!$link) {
-            $output = 'Unable to connect to the Database Server.';
-            include'output.html.php';
-            exit();
-        }
-        if (!mysqli_set_charset($link, 'utf8')) {
-            $output = 'Unable to set database connection encoding.';
-            include'output.html.php';
-            exit();
-        }
-        if (!mysqli_select_db($link, 'kay')) {
+        ?><br>
         
-            include'output.html.php';
-            exit();
-        }
-       
-        include'output.html.php';
-        
-       
-        if(!mysqli_query($link, $sql))
-        {
-           
-            include 'output.html.php';
-            exit();
-        }
-        
-    
-        include 'output.html.php';
-      
-
-$servername = "localhost";
-$username = "kay";
-$password = "dtycts16";
-$dbname = "kay";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-}
-mysqli_close($conn);
-?>
 
         <a href="cart.html.php">Shopping Cart</a>
+        <br>
           <a href="index.php">Home</a>
-          
+          <br>
 
     </body>
 </html>
