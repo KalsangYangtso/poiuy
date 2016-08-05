b488e34edda6605218f6cf03430e07c4466e2e83
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-04 23:04:17 -0400

    changes

--- form.html.php
+++ form.html.php
@@ -70,30 +70,7 @@ Email:</br>
         include 'output.html.php';
       
 
-$servername = "localhost";
-$username = "kay";
-$password = "dtycts16";
-$dbname = "kay";
-$email ="email";
-$cpassword="cpassword";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO CUSTOMER ( email, cpassword)
-VALUES ( " . "\"$email\"" . "," . "\"$cpassword\"" . " )" ;
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
 ?> 
     </body>
 </html>
