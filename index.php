2af041d004e8a232cfe761c6ab116fc28f740309
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-08-07 02:58:53 -0400

    changes

--- index.php
+++ index.php
@@ -24,13 +24,19 @@ if (!$conn) {
 }
 
 
-  $sql = "INSERT INTO CUSTOMER (email, cpassword) VALUES('$email','$cpassword')";
+
+ $sql="INSERT INTO CUSTOMER(email, cpassword ) values('".$_GET['email']."', '".$_GET['cpassword']."')";
+
+$result = mysql_query($sql);
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
+
+
 
 mysqli_close($conn);
 ?> 
